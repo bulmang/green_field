f092a03dea49525b1d72b93786603dbe5e7461f7
bulmang <hmk113737@gmail.com>
2024-10-30 13:59:53 +0900

    [feat]: Implement design system

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
+import 'src/design_system/app_colors.dart';
+import 'src/design_system/app_texts.dart';
+import 'src/extensions/theme_data_extension.dart';
 
 void main() async {
   // Flutter 프레임워크가 초기화되기 전에 Firebase 초기화
@@ -14,32 +17,84 @@ void main() async {
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Firebase App',
+      title: 'Flutter Demo',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primaryColor: AppColorsTheme().gfMainColor,
+        scaffoldBackgroundColor: AppColorsTheme().gfBackGroundColor,
+        textTheme: TextTheme(
+          bodyLarge: TextStyle(color: AppColorsTheme().gfBlackColor),
+          bodyMedium: TextStyle(color: AppColorsTheme().gfGray400Color),
+        ),
+        extensions: [
+          AppColorsTheme(), // Add AppColorsTheme to ThemeData
+          AppTextsTheme.main(), // Add AppTextsTheme to ThemeData
+        ],
       ),
-      home: const MyHomePage(),
+      home: const HomePage(),
     );
   }
 }
 
-class MyHomePage extends StatelessWidget {
-  const MyHomePage({Key? key}) : super(key: key);
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Firebase Example'),
+        title: Text('Home Page', style: TextStyle(color: Theme.of(context).appColors.gfWhiteColor)),
+        backgroundColor: Theme.of(context).appColors.gfMainColor,
       ),
-      body: const Center(
-        child: Text('Welcome to Firebase with Flutter!'),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              '폰트, 색 사용 예시',
+              style: Theme.of(context).appTexts.gfHeading1.copyWith(
+                color: Theme.of(context).appColors.gfBlackColor
+              )
+            ),
+            const SizedBox(height: 20),
+            Container(
+              padding: const EdgeInsets.all(16),
+              decoration: BoxDecoration(
+                color: Theme.of(context).appColors.gfMainBackGroundColor,
+                borderRadius: BorderRadius.circular(8),
+              ),
+              child: Column(
+                children: [
+                  ...List.generate(3, (index) {
+                    return Padding(
+                      padding: const EdgeInsets.only(bottom: 10),
+                      child: Text(
+                        "Heading ${index + 1}",
+                        style: index == 0
+                            ? Theme.of(context).appTexts.gfHeading1.copyWith(
+                          color: Theme.of(context).appColors.gfMainColor,
+                        )
+                            : index == 1
+                            ? Theme.of(context).appTexts.gfHeading2.copyWith(
+                          color: Theme.of(context).appColors.gfBlueColor,
+                        )
+                            : Theme.of(context).appTexts.gfHeading3.copyWith(
+                          color: Theme.of(context).appColors.gfWarningColor,
+                        ),
+                      ),
+                    );
+                  }),
+                ],
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
 }
+
---
+++ lib/src/design_system/app_colors.dart
@@ -0,0 +1,63 @@
+import 'package:flutter/material.dart';
+
+class AppColorsTheme extends ThemeExtension<AppColorsTheme> {
+  final Color gfMainColor; // 02542D
+  final Color gfMainBackGroundColor; // 02542D with 10% opacity
+  final Color gfWarningBackGroundColor; // FF6A69 with 10% opacity
+  final Color gfWarningYellowColor; // FFC35A
+  final Color gfWarningYellowBackGroundColor; // FFC35A with 10% opacity
+  final Color gfWarningColor; // FF6A69
+  final Color gfBlueColor; // 007AFF
+  final Color gfWhiteColor; // FBFDFD
+  final Color gfBackGroundColor; // F4F4F9
+  final Color gfGray100Color; // F3F4F6
+  final Color gfGray300Color; // DBDEE2
+  final Color gfGray400Color; // 8B95A1
+  final Color gfGray800Color; // 535961
+  final Color gfBlackColor; // 000000
+
+  // Private constructor
+  const AppColorsTheme._internal({
+    required this.gfMainColor,
+    required this.gfMainBackGroundColor,
+    required this.gfWarningBackGroundColor,
+    required this.gfWarningYellowColor,
+    required this.gfWarningYellowBackGroundColor,
+    required this.gfWarningColor,
+    required this.gfBlueColor,
+    required this.gfWhiteColor,
+    required this.gfBackGroundColor,
+    required this.gfGray100Color,
+    required this.gfGray300Color,
+    required this.gfGray400Color,
+    required this.gfGray800Color,
+    required this.gfBlackColor,
+  });
+
+  factory AppColorsTheme() {
+    return AppColorsTheme._internal(
+      gfMainColor: const Color(0xFF02542D),
+      gfMainBackGroundColor: const Color(0xFF02542D).withOpacity(0.1),
+      gfWarningBackGroundColor: const Color(0xFFFF6A69).withOpacity(0.1),
+      gfWarningYellowColor: const Color(0xFFFFC35A),
+      gfWarningYellowBackGroundColor: const Color(0xFFFFC35A).withOpacity(0.1),
+      gfWarningColor: const Color(0xFFFF6A69),
+      gfBlueColor: const Color(0xFF007AFF),
+      gfWhiteColor: const Color(0xFFFBFBFD),
+      gfBackGroundColor: const Color(0xFFF4F4F9),
+      gfGray100Color: const Color(0xFFF3F4F6),
+      gfGray300Color: const Color(0xFFDBDEE2),
+      gfGray400Color: const Color(0xFF8B95A1),
+      gfGray800Color: const Color(0xFF535961),
+      gfBlackColor: const Color(0xFF000000),
+    );
+  }
+
+  @override
+  AppColorsTheme copyWith() {
+    return AppColorsTheme();
+  }
+
+  @override
+  AppColorsTheme lerp(covariant ThemeExtension<AppColorsTheme>? other, double t) => this;
+}
