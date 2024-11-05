d700c9388e5fa5e4b84f0a91e31cffa8f2b0888d
bulmang <hmk113737@gmail.com>
2024-11-05 11:16:09 +0900

    [feat]: Add GreenFieldCommentWidget component

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:green_field/src/components/greenfield_comment_widget.dart';
 import 'package:green_field/src/components/greenfield_content_widget.dart';
 import 'package:green_field/src/components/greenfield_text_field.dart';
 import 'firebase_options.dart';
@@ -66,30 +67,46 @@ class SamplePage extends StatelessWidget {
           ),
         ],
       ),
-      body: GreenFieldContentWidget(
-          title: "제목 예시",
-          bodyText:
-          '''
-      안녕하세요 서울 경제 진흥원 관악캠퍼스 매니저입니다.
-      최근 식당 늘어난 걸 모르시는 분들이 많은 것 같다고 하셔서 (저도 몰랐네용) 홍보차 메세지 남깁니다!!
-      안녕하세요 서울 경제 진흥원 관악캠퍼스 매니저입니다. 최근 식당 늘어난 걸 모르시는 분들이 많은 것 같다고 하셔서 
-      (저도 몰랐네용) 홍보차 메세지 남깁니다!! 안녕하세요 서울 경제 진흥원 관악캠퍼스 매니저입니다. 
-      최근 식당 늘어난 걸 모르시는 분들이 많은 것 같다고 하셔서 (저도 몰랐네용) 홍보차 메세지 남깁니다!!안녕하세요 
-      서울 경제 진흥원 관악캠퍼스 매니저입니다. 최근 식당 늘어난 걸 모르시는 분들이 많은 것 같다고 하셔서 (저도 몰랐네용)
-      (저도 몰랐네용) 홍보차 메세지 남깁니다!! 안녕하세요 서울 경제 진흥원 관악캠퍼스 매니저입니다. 
-      최근 식당 늘어난 걸 모르시는 분들이 많은 것 같다고 하셔서 (저도 몰랐네용) 홍보차 메세지 남깁니다!!안녕하세요 
-      서울 경제 진흥원 관악캠퍼스 매니저입니다. 최근 식당 늘어난 걸 모르시는 분들이 많은 것 같다고 하셔서 (저도 몰랐네용) 
-      홍보차 메세지 남깁니다!!안녕하세요 서울 경제 진흥원 관악캠퍼스 매니저입니다. 최근 식당 늘어난 걸 모르시는 분들이 많은 것 같다고 하셔서
-      (저도 몰랐네용) 홍보차 메세지 남깁니다!!
-      (저도 몰랐네용) 홍보차 메세지 남깁니다!!
-      (저도 몰랐네용) 홍보차 메세지 남깁니다!!
-      (저도 몰랐네용) 홍보차 메세지 남깁니다!!'''
-          ,
-          imageAssets: [
-            "https://images.dog.ceo/breeds/spitz-japanese/beet-001.jpg"
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            GreenFieldContentWidget(
+                title: "제목 예시",
+                bodyText:
+                '''
+안녕하세요 서울 경제 진흥원 관악캠퍼스 매니저입니다.
+최근 식당 늘어난 걸 모르시는 분들이 많은 것 같다고 하셔서 (저도 몰랐네용) 홍보차 메세지 남깁니다!!
+안녕하세요 서울 경제 진흥원 관악캠퍼스 매니저입니다. 최근 식당 늘어난 걸 모르시는 분들이 많은 것 같다고 하셔서 
+(저도 몰랐네용) 홍보차 메세지 남깁니다!! 안녕하세요 서울 경제 진흥원 관악캠퍼스 매니저입니다. 
+최근 식당 늘어난 걸 모르시는 분들이 많은 것 같다고 하셔서 (저도 몰랐네용) 홍보차 메세지 남깁니다!!안녕하세요 
+서울 경제 진흥원 관악캠퍼스 매니저입니다. 최근 식당 늘어난 걸 모르시는 분들이 많은 것 같다고 하셔서 (저도 몰랐네용)
+(저도 몰랐네용) 홍보차 메세지 남깁니다!! 안녕하세요 서울 경제 진흥원 관악캠퍼스 매니저입니다. 
+최근 식당 늘어난 걸 모르시는 분들이 많은 것 같다고 하셔서 (저도 몰랐네용) 홍보차 메세지 남깁니다!!안녕하세요 
+서울 경제 진흥원 관악캠퍼스 매니저입니다. 최근 식당 늘어난 걸 모르시는 분들이 많은 것 같다고 하셔서 (저도 몰랐네용) 
+홍보차 메세지 남깁니다!!안녕하세요 서울 경제 진흥원 관악캠퍼스 매니저입니다. 최근 식당 늘어난 걸 모르시는 분들이 많은 것 같다고 하셔서
+(저도 몰랐네용) 홍보차 메세지 남깁니다!!
+(저도 몰랐네용) 홍보차 메세지 남깁니다!!
+(저도 몰랐네용) 홍보차 메세지 남깁니다!!
+(저도 몰랐네용) 홍보차 메세지 남깁니다!!'''
+                ,
+                imageAssets: [
+                  "https://images.dog.ceo/breeds/spitz-japanese/beet-001.jpg"
+                ],
+              likes: 14,
+              commentCount: 0,
+            ),
+            GreenFieldCommentWidget(
+              campus: '관악캠퍼스',
+              dateTime: DateTime.parse('2024-11-05 14:30:00'),
+              comment: '저는 이렇게 하고 있습니다.저는 이렇게 하고 있습니다.저는 이렇게 하고 있습니다.저는 이렇게 하고 있습니다.',
+            ),
+            GreenFieldCommentWidget(
+              campus: '관악캠퍼스',
+              dateTime: DateTime.parse('2024-11-05 14:30:00'),
+              comment: '저는 이렇게 하고 있습니다.저는 이렇게 하고 있습니다.저는 이렇게 하고 있습니다.저는 이렇게 하고 있습니다.',
+            )
           ],
-        likes: 14,
-        commentCount: 0,
+        ),
       ),
       bottomNavigationBar: GreenFieldTextField(
         type: TextFieldType.post,
---
+++ lib/src/components/greenfield_comment_widget.dart
@@ -0,0 +1,73 @@
+import 'package:flutter/material.dart';
+import 'package:green_field/src/design_system/app_colors.dart';
+import 'package:green_field/src/design_system/app_icons.dart';
+
+import '../design_system/app_texts.dart';
+
+class GreenFieldCommentWidget extends StatelessWidget {
+  final String campus;
+  final DateTime dateTime;
+  final String comment;
+
+  const GreenFieldCommentWidget({
+    super.key,
+    required this.campus,
+    required this.dateTime,
+    required this.comment,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Divider(
+          height: 1,
+          color: AppColorsTheme().gfGray300Color,
+        ),
+        SizedBox(height: 8),
+        Container(
+          padding: EdgeInsets.only(left: 30, right: 16, bottom: 8),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Row(
+                children: [
+                  Image.asset(AppIcons.profile, width: 30, height: 30),
+                  SizedBox(width: 4),
+                  Expanded(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          campus,
+                          style: AppTextsTheme.main().gfBody5.copyWith(
+                            color: AppColorsTheme().gfBlackColor,
+                          ),
+                        ),
+                        SizedBox(height: 2),
+                        Text(
+                          '${dateTime.month}/${dateTime.day} ${dateTime.hour}:${dateTime.minute.toString().padLeft(2, '0')}',
+                          style: AppTextsTheme.main().gfCaption5.copyWith(
+                            color: AppColorsTheme().gfGray400Color,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+              SizedBox(height: 5),
+              Text(
+                  comment.length > 300 ? '${comment.substring(0, 300)}...' : comment,
+                  style: AppTextsTheme.main().gfBody5.copyWith(
+                    color: AppColorsTheme().gfBlackColor,
+                  )
+              ),
+            ],
+          ),
+        ),
+      ],
+    );
+  }
+}
