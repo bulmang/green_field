b9edb914ee9d154bc6b295fb4eacd3db16b69ebc
bulmang <hmk113737@gmail.com>
2024-11-05 16:20:59 +0900

    [feat]: Add GreenFieldEditSection component
    - 사진에서 여러 이미지들 가져오기
    - 이미지 삭제 기능

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,9 @@ import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:green_field/src/components/greenfield_comment_widget.dart';
 import 'package:green_field/src/components/greenfield_content_widget.dart';
+import 'package:green_field/src/components/greenfield_edit_section.dart';
 import 'package:green_field/src/components/greenfield_text_field.dart';
+import 'package:green_field/src/enums/feature_type.dart';
 import 'firebase_options.dart';
 import 'package:green_field/src/components/greenfield_confirm_button.dart';
 import 'package:green_field/src/components/greenfield_app_bar.dart';
@@ -67,53 +69,7 @@ class SamplePage extends StatelessWidget {
           ),
         ],
       ),
-      body: SingleChildScrollView(
-        child: Column(
-          children: [
-            GreenFieldContentWidget(
-                title: "제목 예시",
-                bodyText:
-                '''
-안녕하세요 서울 경제 진흥원 관악캠퍼스 매니저입니다.
-최근 식당 늘어난 걸 모르시는 분들이 많은 것 같다고 하셔서 (저도 몰랐네용) 홍보차 메세지 남깁니다!!
-안녕하세요 서울 경제 진흥원 관악캠퍼스 매니저입니다. 최근 식당 늘어난 걸 모르시는 분들이 많은 것 같다고 하셔서 
-(저도 몰랐네용) 홍보차 메세지 남깁니다!! 안녕하세요 서울 경제 진흥원 관악캠퍼스 매니저입니다. 
-최근 식당 늘어난 걸 모르시는 분들이 많은 것 같다고 하셔서 (저도 몰랐네용) 홍보차 메세지 남깁니다!!안녕하세요 
-서울 경제 진흥원 관악캠퍼스 매니저입니다. 최근 식당 늘어난 걸 모르시는 분들이 많은 것 같다고 하셔서 (저도 몰랐네용)
-(저도 몰랐네용) 홍보차 메세지 남깁니다!! 안녕하세요 서울 경제 진흥원 관악캠퍼스 매니저입니다. 
-최근 식당 늘어난 걸 모르시는 분들이 많은 것 같다고 하셔서 (저도 몰랐네용) 홍보차 메세지 남깁니다!!안녕하세요 
-서울 경제 진흥원 관악캠퍼스 매니저입니다. 최근 식당 늘어난 걸 모르시는 분들이 많은 것 같다고 하셔서 (저도 몰랐네용) 
-홍보차 메세지 남깁니다!!안녕하세요 서울 경제 진흥원 관악캠퍼스 매니저입니다. 최근 식당 늘어난 걸 모르시는 분들이 많은 것 같다고 하셔서
-(저도 몰랐네용) 홍보차 메세지 남깁니다!!
-(저도 몰랐네용) 홍보차 메세지 남깁니다!!
-(저도 몰랐네용) 홍보차 메세지 남깁니다!!
-(저도 몰랐네용) 홍보차 메세지 남깁니다!!'''
-                ,
-                imageAssets: [
-                  "https://images.dog.ceo/breeds/spitz-japanese/beet-001.jpg"
-                ],
-              likes: 14,
-              commentCount: 0,
-            ),
-            GreenFieldCommentWidget(
-              campus: '관악캠퍼스',
-              dateTime: DateTime.parse('2024-11-05 14:30:00'),
-              comment: '저는 이렇게 하고 있습니다.저는 이렇게 하고 있습니다.저는 이렇게 하고 있습니다.저는 이렇게 하고 있습니다.',
-            ),
-            GreenFieldCommentWidget(
-              campus: '관악캠퍼스',
-              dateTime: DateTime.parse('2024-11-05 14:30:00'),
-              comment: '저는 이렇게 하고 있습니다.저는 이렇게 하고 있습니다.저는 이렇게 하고 있습니다.저는 이렇게 하고 있습니다.',
-            )
-          ],
-        ),
-      ),
-      bottomNavigationBar: GreenFieldTextField(
-        type: TextFieldType.post,
-        onAction: (text) {
-          print('전송된 텍스트: $text');
-        },
-      ),
+      body: GreenFieldEditSection(type: FeatureType.recruit),
     );
   }
 }
---
+++ lib/src/enums/recruit_setting_type.dart
@@ -0,0 +1,5 @@
+enum RecruitSettingType {
+  autoDeleteTime,
+  currentPerson,
+  maxPerson,
+}
