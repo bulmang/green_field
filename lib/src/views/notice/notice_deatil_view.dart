42f9997f49ef9744ee637e1223612db65e688c3f
bulmang <hmk113737@gmail.com>
2024-11-14 14:47:32 +0900

    [add]: Add SingleChildScrollView

--- lib/src/views/notice/notice_deatil_view.dart
+++ lib/src/views/notice/notice_deatil_view.dart
@@ -4,6 +4,7 @@ import 'package:green_field/src/components/greenfield_app_bar.dart';
 import 'package:green_field/src/components/greenfield_content_widget.dart';
 import 'package:green_field/src/components/greenfield_user_info_widget.dart';
 import 'package:green_field/src/design_system/app_colors.dart';
+import 'package:green_field/src/enums/feature_type.dart';
 
 import '../../model/notice.dart';
 
@@ -34,25 +35,27 @@ class _NoticeDetailViewState extends State<NoticeDetailView> {
           },
         ),
       ),
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.start, // 왼쪽 정렬
-        children: [
-          Padding(
-            padding: const EdgeInsets.symmetric(vertical: 16.0),
-            child: GreenfieldUserInfoWidget(
-              type: 'notice',
-              campus: notice.userCampus,
-              createTimeText: '${notice.createdAt.year}-${notice.createdAt.month}-${notice.createdAt.day}',
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start, // 왼쪽 정렬
+          children: [
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 16.0),
+              child: GreenfieldUserInfoWidget(
+                featureType: FeatureType.notice,
+                campus: notice.userCampus,
+                createTimeText: '${notice.createdAt.year}-${notice.createdAt.month}-${notice.createdAt.day}',
+              ),
             ),
-          ),
-          GreenFieldContentWidget(
-            title: notice.title,
-            bodyText: notice.body,
-            imageAssets: notice.images != null && notice.images!.isNotEmpty ? notice.images! : [],
-            likes: notice.like.length,
-            commentCount: 0,
-          ),
-        ],
+            GreenFieldContentWidget(
+              title: notice.title,
+              bodyText: notice.body,
+              imageAssets: notice.images != null && notice.images!.isNotEmpty ? notice.images! : [],
+              likes: notice.like.length,
+              commentCount: 0,
+            ),
+          ],
+        ),
       ),
     );
   }
