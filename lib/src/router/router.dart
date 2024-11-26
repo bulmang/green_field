c1388bfd607f8ecab950e18e8c08378db7081e6b
bulmang <hmk113737@gmail.com>
2024-11-26 14:04:46 +0900

    [feat]: Implement navigation using GoRouter

---
+++ lib/src/router/router.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/cupertino.dart';
+import 'package:go_router/go_router.dart';
+import 'package:green_field/src/views/main_view.dart';
+import 'package:green_field/src/views/recruitment/recruitment_detail_view.dart';
+
+import '../model/recruit.dart';
+import '../viewmodels/recruit_view_model.dart';
+
+final recruitVM = RecruitViewModel();
+
+final router = GoRouter(
+  routes: [
+    GoRoute(
+      path: '/',
+      builder: (context, state) => MainView(),
+      routes: [
+        GoRoute(
+          name: "RecruitDetail",
+          path: '/recruitdetail/:id', // recruit ID를 URL 파라미터로 사용
+          pageBuilder: (context, state) {
+            return CustomTransitionPage(
+              key: state.pageKey,
+              child: RecruitDetailView(recruit: recruitVM.getRecruitById(state.pathParameters['id']!)),
+              transitionsBuilder: (context, animation, secondaryAnimation, child) {
+                const begin = Offset(0.0, 1.0); // 아래에서 시작
+                const end = Offset.zero; // 현재 위치
+                const curve = Curves.easeInOut;
+
+                var tween = Tween(begin: begin, end: end).chain(CurveTween(curve: curve));
+                var offsetAnimation = animation.drive(tween);
+
+                return SlideTransition(
+                  position: offsetAnimation,
+                  child: child,
+                );
+              },
+            );
+          },
+        ),
+      ],
+    ),
+  ],
+);
--- lib/src/views/recruitment/recruitment_view.dart
+++ lib/src/views/recruitment/recruitment_view.dart
@@ -1,13 +1,12 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 import 'package:green_field/src/components/greenfield_app_bar.dart';
 import 'package:green_field/src/components/greenfield_recruit_list.dart'; // Import your GreenFieldRecruitList
 import 'package:green_field/src/design_system/app_colors.dart';
-import 'package:green_field/src/enums/feature_type.dart';
 import 'package:green_field/src/views/recruitment/recruit_edit_view.dart';
-import 'package:green_field/src/views/recruitment/recruitment_detail_view.dart';
 
-import '../../model/recruit.dart'; // Assuming you have a Recruit model
+import '../../viewmodels/recruit_view_model.dart';
 
 class RecruitView extends StatefulWidget {
   const RecruitView({super.key});
@@ -17,96 +16,7 @@ class RecruitView extends StatefulWidget {
 }
 
 class _RecruitViewState extends State<RecruitView> {
-  final List<Recruit> recruits = [
-    Recruit(
-      id: '1',
-      creatorId: 'user_001',
-      remainTime: 60, // Remaining time in minutes (1 hour)
-      currentParticipants: ['user_002', 'user_003'],
-      maxParticipants: 4,
-      creatorCampus: '캠퍼스 A',
-      isEntryAvailable: true,
-      isTimeExpired: false,
-      title: '여름 인턴 모집',
-      body: '여름 인턴을 모집합니다. 많은 지원 바랍니다.',
-      images: [
-        'https://images.dog.ceo/breeds/pitbull/dog-3981033_1280.jpg',
-      ],
-      createdAt: DateTime.now().subtract(Duration(days: 1)),
-    ),
-    Recruit(
-      id: '2',
-      creatorId: 'user_002',
-      remainTime: 120, // Remaining time in minutes (2 hours)
-      currentParticipants: [],
-      maxParticipants: 4,
-      creatorCampus: '캠퍼스 B',
-      isEntryAvailable: true,
-      isTimeExpired: false,
-      title: '동아리 회원 모집',
-      body: '우리 동아리에 가입하세요!',
-      images: null,
-      createdAt: DateTime.now().subtract(Duration(days: 2)),
-    ),
-    Recruit(
-      id: '3',
-      creatorId: 'user_003',
-      remainTime: 30, // Remaining time in minutes (30 minutes)
-      currentParticipants: ['user_004'], // Minimum 1 participant
-      maxParticipants: 4,
-      creatorCampus: '캠퍼스 C',
-      isEntryAvailable: true,
-      isTimeExpired: true, // Time expired because remainTime is less than 30 minutes
-      title: '여름 캠프 참가자 모집',
-      body: '여름 캠프에 참여할 학생을 모집합니다!',
-      images: [],
-      createdAt: DateTime.now().subtract(Duration(days: 1)),
-    ),
-    Recruit(
-      id: '4',
-      creatorId: 'user_004',
-      remainTime: 90, // Remaining time in minutes (1 hour 30 minutes)
-      currentParticipants: ['user_005', 'user_006'], // 2 participants
-      maxParticipants: 4,
-      creatorCampus: '캠퍼스 D',
-      isEntryAvailable: true,
-      isTimeExpired: false, // Time not expired
-      title: '프로젝트 팀원 모집',
-      body: '프로젝트 팀원을 모집합니다. 많은 지원 바랍니다.',
-      images: [
-        'https://images.dog.ceo/breeds/pitbull/dog-3981033_1280.jpg',
-      ],
-      createdAt: DateTime.now().subtract(Duration(days: 2)),
-    ),
-    Recruit(
-      id: '5',
-      creatorId: 'user_005',
-      remainTime: 40, // Remaining time in minutes
-      currentParticipants: ['user_007', 'user_008', 'user_009'], // 3 participants
-      maxParticipants: 4,
-      creatorCampus: '캠퍼스 E',
-      isEntryAvailable: false, // Current participants equal to max participants
-      isTimeExpired: false, // Time not expired
-      title: '여름 방학 프로그램 모집',
-      body: '여름 방학 프로그램에 참여할 학생을 모집합니다.',
-      images: null,
-      createdAt: DateTime.now().subtract(Duration(days: 3)),
-    ),
-    Recruit(
-      id: '6',
-      creatorId: 'user_006',
-      remainTime: 60, // Remaining time in minutes
-      currentParticipants: ['user_010', 'user_011', 'user_012', 'user_013'], // 4 participants
-      maxParticipants: 4,
-      creatorCampus: '캠퍼스 F',
-      isEntryAvailable: false, // Current participants equal to max participants
-      isTimeExpired: false, // Time not expired
-      title: '여름 캠프 팀원 모집',
-      body: '여름 캠프에 참여할 팀원을 모집합니다.',
-      images: null,
-      createdAt: DateTime.now().subtract(Duration(days: 4)),
-    ),
-  ];
+
 
   @override
   Widget build(BuildContext context) {
@@ -148,31 +58,13 @@ class _RecruitViewState extends State<RecruitView> {
         ],
       ),
       body: ListView.builder(
-        itemCount: recruits.length,
+        itemCount: RecruitViewModel().recruits.length,
         itemBuilder: (context, index) {
-          final recruit = recruits[index];
+          final recruit = RecruitViewModel().recruits[index];
           return GreenFieldRecruitList(
             recruits: recruit,
             onTap: () {
-              Navigator.push(
-                context,
-                PageRouteBuilder(
-                  pageBuilder: (context, animation, secondaryAnimation) => RecruitDetailView(recruit: recruit),
-                  transitionsBuilder: (context, animation, secondaryAnimation, child) {
-                    const begin = Offset(0.0, 1.0);
-                    const end = Offset.zero;
-                    const curve = Curves.easeInOut;
-
-                    var tween = Tween(begin: begin, end: end).chain(CurveTween(curve: curve));
-                    var offsetAnimation = animation.drive(tween);
-
-                    return SlideTransition(
-                      position: offsetAnimation,
-                      child: child,
-                    );
-                  },
-                ),
-              );
+              context.go('/recruitdetail/${recruit.id}');
             },
           );
         },
