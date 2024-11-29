ad0ae906aaf0bb9366ae7543fbea0714440c4c27
bulmang <hmk113737@gmail.com>
2024-11-29 22:12:48 +0900

    [feat]: Implement Post Edit View UI

---
+++ lib/src/views/post/post_edit_view.dart
@@ -0,0 +1,65 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import 'package:green_field/src/components/greenfield_app_bar.dart';
+import 'package:green_field/src/components/greenfield_edit_section.dart';
+import 'package:green_field/src/design_system/app_colors.dart';
+import 'package:green_field/src/enums/feature_type.dart';
+import '../../design_system/app_texts.dart';
+
+class PostEditView extends StatefulWidget {
+  const PostEditView({super.key});
+
+  @override
+  _PostEditViewState createState() => _PostEditViewState();
+}
+
+class _PostEditViewState extends State<PostEditView> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: AppColorsTheme().gfWhiteColor,
+      appBar: GreenFieldAppBar(
+        backgGroundColor: AppColorsTheme().gfWhiteColor,
+        title: "모집글 쓰기",
+        leadingIcon: Icon(
+          CupertinoIcons.xmark,
+          color: AppColorsTheme().gfGray400Color,
+        ),
+        leadingAction: () {
+          context.pop();
+        },
+        actions: [
+          CupertinoButton(
+            onPressed: () {
+              context.pop();
+            },
+            child: Container(
+              decoration: BoxDecoration(
+                gradient: LinearGradient(
+                  colors: [Color(0xFF308F5B), Color(0xFF666666)],
+                  begin: Alignment.centerLeft,
+                  end: Alignment.centerRight,
+                ),
+                borderRadius: BorderRadius.circular(30),
+              ),
+              height: 30,
+              child: Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 10,vertical: 2),
+                child: Text(
+                  '완료',
+                  style: AppTextsTheme.main().gfCaption2.copyWith(
+                    color: AppColorsTheme().gfWhiteColor,
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+      body: GreenFieldEditSection(
+        type: FeatureType.post,
+      ),
+    );
+  }
+}
