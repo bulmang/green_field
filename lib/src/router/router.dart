ebc861a405430ee87e4f611ebac5d4a5dd96e46a
bulmang <hmk113737@gmail.com>
2024-11-29 22:23:01 +0900

    feat: Update routing structure and integrate ViewModels
    - StatefulNavigationShell을 추가하여 네비게이션 상태를 관리
    - NoticeViewModel, RecruitViewModel, PostViewModel을 통합하여 데이터 처리를 수행
    - 공지사항, 모집, 게시물 세부 정보에 대한 경로를 구현하고 딥 링크 지원을 추가합니다.
    - StatefulShellBranch로 구성하여 더 나은 네비게이션 관리를 제공합니다.

--- lib/src/router/router.dart
+++ lib/src/router/router.dart
@@ -1,141 +1,192 @@
 import 'package:flutter/cupertino.dart';
 import 'package:go_router/go_router.dart';
 import 'package:green_field/src/viewmodels/post_view_model.dart';
+import 'package:green_field/src/views/campus/campus_view.dart';
+import 'package:green_field/src/views/home/home_view.dart';
+import 'package:green_field/src/views/login/login_view.dart';
 import 'package:green_field/src/views/main_view.dart';
 import 'package:green_field/src/views/notice/notice_view.dart';
 import 'package:green_field/src/views/notice/notice_deatil_view.dart';
 import 'package:green_field/src/views/post/post_detail_view.dart';
+import 'package:green_field/src/views/post/post_edit_view.dart';
 import 'package:green_field/src/views/recruitment/recruit_edit_view.dart';
 import 'package:green_field/src/views/recruitment/recruitment_detail_view.dart';
+import 'package:green_field/src/views/recruitment/recruitment_view.dart';
 
 import '../viewmodels/notice_view_model.dart';
 import '../viewmodels/recruit_view_model.dart';
+import '../views/post/post_view.dart';
 
 final noticeVM = NoticeViewModel();
 final recruitVM = RecruitViewModel();
 final postVM = PostViewModel();
 
-final router = GoRouter(
-  routes: [
+final GlobalKey<NavigatorState> _rootNavigatorKey = GlobalKey<NavigatorState>(debugLabel: 'root');
+final GlobalKey<NavigatorState> _homeTabNavigatorKey = GlobalKey<NavigatorState>(debugLabel: 'homeTab');
+final GlobalKey<NavigatorState> _recruitTabNavigatorKey = GlobalKey<NavigatorState>(debugLabel: 'recruitTab');
+final GlobalKey<NavigatorState> _postTabNavigatorKey = GlobalKey<NavigatorState>(debugLabel: 'postTab');
+final GlobalKey<NavigatorState> _campusTabNavigatorKey = GlobalKey<NavigatorState>(debugLabel: 'campusTab');
+
+final GoRouter router = GoRouter(
+  navigatorKey: _rootNavigatorKey,
+  routes: <RouteBase>[
     GoRoute(
       path: '/',
-      builder: (context, state) => MainView(),
-      routes: [
-        /// Notice
-        GoRoute(
-          name: "Notice",
-          path: '/notice',
-          builder: (context, state) => const NoticeView(),
-        ),
-        GoRoute(
-          name: "NoticeDetail",
-          path: 'noticedetail/:id',
-          builder: (context, state) => NoticeDetailView(notice: noticeVM.getNoticeById(state.pathParameters['id']!)),
-        ),
-
-        /// Recruit
-        GoRoute(
-          name: "RecruitDetail",
-          path: 'recruitdetail/:id', // recruit ID를 URL 파라미터로 사용
-          pageBuilder: (context, state) {
-            return CustomTransitionPage(
-              key: state.pageKey,
-              child: RecruitDetailView(recruit: recruitVM.getRecruitById(state.pathParameters['id']!)),
-              transitionsBuilder: (context, animation, secondaryAnimation, child) {
-                const begin = Offset(0.0, 1.0); // 아래에서 시작
-                const end = Offset.zero; // 현재 위치
-                const curve = Curves.easeInOut;
-
-                var tween = Tween(begin: begin, end: end).chain(CurveTween(curve: curve));
-                var offsetAnimation = animation.drive(tween);
-
-                return SlideTransition(
-                  position: offsetAnimation,
-                  child: child,
-                );
-              },
-            );
-          },
-        ),
-        GoRoute(
-          name: "RecruitEdit",
-          path: 'recruitedit',
-          pageBuilder: (context, state) {
-            return CustomTransitionPage(
-              key: state.pageKey,
-              child: RecruitEditView(),
-              transitionsBuilder: (context, animation, secondaryAnimation, child) {
-                const begin = Offset(0.0, 1.0); // 아래에서 시작
-                const end = Offset.zero; // 현재 위치
-                const curve = Curves.easeInOut;
-
-                var tween = Tween(begin: begin, end: end).chain(CurveTween(curve: curve));
-                var offsetAnimation = animation.drive(tween);
-
-                return SlideTransition(
-                  position: offsetAnimation,
-                  child: child,
-                );
-              },
-            );
-          },
-        ),
-
-        GoRoute(
-          name: "PostDetail",
-          path: 'postdetail/:id',
-          builder: (context, state) => PostDetailView(post: postVM.getPostById(state.pathParameters['id']!)),
-        ),
-        GoRoute(
-          name: "PostEdit",
-          path: 'postedit',
-          pageBuilder: (context, state) {
-            return CustomTransitionPage(
-              key: state.pageKey,
-              child: RecruitEditView(),
-              transitionsBuilder: (context, animation, secondaryAnimation, child) {
-                const begin = Offset(0.0, 1.0); // 아래에서 시작
-                const end = Offset.zero; // 현재 위치
-                const curve = Curves.easeInOut;
-
-                var tween = Tween(begin: begin, end: end).chain(CurveTween(curve: curve));
-                var offsetAnimation = animation.drive(tween);
-
-                return SlideTransition(
-                  position: offsetAnimation,
-                  child: child,
-                );
-              },
-            );
-          },
-        ),
-
-        /// DeepLink
-        GoRoute(
-          name: "NoticeDeepLink",
-          path: 'notice',
-          builder: (context, state) =>  NoticeView(),
+      builder: (context, state) => LoginView(),
+    ),
+    StatefulShellRoute.indexedStack(
+      parentNavigatorKey: _rootNavigatorKey,
+      builder: (context, state, navigationShell) {
+        return MainView(navigationShell: navigationShell);
+      },
+      branches: [
+        StatefulShellBranch(
+          navigatorKey: _homeTabNavigatorKey,
           routes: [
             GoRoute(
-              name: 'notice_detail',
-              path: 'detail/:id',
-              builder: (context, state) =>  NoticeDetailView(notice: noticeVM.getNoticeById(state.pathParameters['id']!)),
+              path: '/home',
+              builder: (BuildContext context, GoRouterState state) {
+                return HomeView();
+              },
+              routes: <RouteBase>[
+                GoRoute(
+                  path: 'notice',
+                  builder: (context, state) => NoticeView(),
+                  routes: <RouteBase>[
+                    GoRoute(
+                      name: 'notice_detail',
+                      path: 'detail/:id',
+                      builder: (context, state) => NoticeDetailView(
+                          notice: noticeVM
+                              .getNoticeById(state.pathParameters['id']!)),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ],
+        ),
+        StatefulShellBranch(
+          navigatorKey: _recruitTabNavigatorKey,
+          routes: [
+            GoRoute(
+              path: '/recruit',
+              builder: (BuildContext context, GoRouterState state) {
+                return RecruitView();
+              },
+              routes: <RouteBase>[
+                GoRoute(
+                  name: 'recruit_detial',
+                  path: 'detail/:id',
+                  pageBuilder: (context, state) {
+                    return CustomTransitionPage(
+                      key: state.pageKey,
+                      child: RecruitDetailView(
+                          recruit: recruitVM
+                              .getRecruitById(state.pathParameters['id']!)),
+                      transitionsBuilder:
+                          (context, animation, secondaryAnimation, child) {
+                        const begin = Offset(0.0, 1.0); // 아래에서 시작
+                        const end = Offset.zero; // 현재 위치
+                        const curve = Curves.easeInOut;
+
+                        var tween = Tween(begin: begin, end: end)
+                            .chain(CurveTween(curve: curve));
+                        var offsetAnimation = animation.drive(tween);
+
+                        return SlideTransition(
+                          position: offsetAnimation,
+                          child: child,
+                        );
+                      },
+                    );
+                  },
+                ),
+                GoRoute(
+                  name: 'recruit_edit',
+                  path: 'edit',
+                  pageBuilder: (context, state) {
+                    return CustomTransitionPage(
+                      key: state.pageKey,
+                      child: RecruitEditView(),
+                      transitionsBuilder:
+                          (context, animation, secondaryAnimation, child) {
+                        const begin = Offset(0.0, 1.0); // 아래에서 시작
+                        const end = Offset.zero; // 현재 위치
+                        const curve = Curves.easeInOut;
+
+                        var tween = Tween(begin: begin, end: end)
+                            .chain(CurveTween(curve: curve));
+                        var offsetAnimation = animation.drive(tween);
+
+                        return SlideTransition(
+                          position: offsetAnimation,
+                          child: child,
+                        );
+                      },
+                    );
+                  },
+                ),
+              ],
             )
           ],
         ),
-        GoRoute(
-          name: "PostDeepLink",
-          path: 'post',
-          builder: (context, state) =>  MainView(),
+        StatefulShellBranch(
+          navigatorKey: _postTabNavigatorKey,
           routes: [
             GoRoute(
-              name: 'post_detail',
-              path: 'detail/:id',
-              builder: (context, state) =>  PostDetailView(post: postVM.getPostById(state.pathParameters['id']!)),
-            )
+              path: '/post',
+              builder: (BuildContext context, GoRouterState state) {
+                return PostView();
+              },
+              routes: <RouteBase>[
+                GoRoute(
+                  name: 'post_detail',
+                  path: 'detail/:id',
+                  builder: (context, state) => PostDetailView(
+                      post: postVM.getPostById(state.pathParameters['id']!)),
+                ),
+                GoRoute(
+                  name: "post_edit",
+                  path: 'edit',
+                  pageBuilder: (context, state) {
+                    return CustomTransitionPage(
+                      key: state.pageKey,
+                      child: PostEditView(),
+                      transitionsBuilder:
+                          (context, animation, secondaryAnimation, child) {
+                        const begin = Offset(0.0, 1.0); // 아래에서 시작
+                        const end = Offset.zero; // 현재 위치
+                        const curve = Curves.easeInOut;
+
+                        var tween = Tween(begin: begin, end: end)
+                            .chain(CurveTween(curve: curve));
+                        var offsetAnimation = animation.drive(tween);
+
+                        return SlideTransition(
+                          position: offsetAnimation,
+                          child: child,
+                        );
+                      },
+                    );
+                  },
+                ),
+              ],
+            ),
+          ],
+        ),
+        StatefulShellBranch(
+          navigatorKey: _campusTabNavigatorKey,
+          routes: [
+            GoRoute(
+              path: '/campus',
+              builder: (BuildContext context, GoRouterState state) {
+                return CampusView();
+              },
+            ),
           ],
         ),
       ],
-    ),
+    )
   ],
 );
