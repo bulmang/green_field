d9fdff5397862cc951ccc8d6b1f7ba11fd544d73
bulmang <hmk113737@gmail.com>
2025-01-10 13:31:57 +0900

    [name]: change User property name

--- lib/src/model/user.dart
+++ lib/src/model/user.dart
@@ -1,34 +1,37 @@
 class User {
   String id;
-  String simpleLoginId;
+  String providerId;
   String userType;
   String campus;
   String course;
   String name;
   DateTime? createDate;
-  DateTime? lastLoginDate;
+  DateTime? lastSignInDate;
+  DateTime? lastLoginInDate;
 
   User({
     required this.id,
-    required this.simpleLoginId,
+    required this.providerId,
     this.userType = "student",
     required this.campus,
     required this.course,
     required this.name,
     this.createDate,
-    this.lastLoginDate,
+    this.lastSignInDate,
+    this.lastLoginInDate,
   });
 
   Map<String, dynamic> toMap() {
     return {
       'id': id,
-      'simple_login_id': simpleLoginId,
+      'simple_login_id': providerId,
       'user_type': userType,
       'campus': campus,
       'course': course,
       'name': name,
       'create_date': createDate?.toIso8601String(),
-      'last_login_date': lastLoginDate?.toIso8601String(),
+      'last_signin_date': lastSignInDate?.toIso8601String(),
+      'last_login_date': lastSignInDate?.toIso8601String(),
     };
   }
 
@@ -42,13 +45,14 @@ class User {
 
     return User(
       id: id,
-      simpleLoginId: simpleLoginId,
+      providerId: simpleLoginId,
       userType: map['user_type'] ?? "student",
       campus: campus,
       course: course,
       name: name,
       createDate: map['create_date'] != null ? DateTime.parse(map['create_date']) : null,
-      lastLoginDate: map['last_login_date'] != null ? DateTime.parse(map['last_login_date']) : null,
+      lastSignInDate: map['last_signin_date'] != null ? DateTime.parse(map['last_signin_date']) : null,
+      lastLoginInDate: map['last_login_date'] != null ? DateTime.parse(map['last_login_date']) : null,
     );
   }
 }
