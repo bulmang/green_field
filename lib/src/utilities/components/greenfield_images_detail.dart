4d7828142d53966b6244d4e434d3cc97a251cfcb
bulmang <hmk113737@gmail.com>
2025-02-17 22:23:19 +0900

    [feat]: Add image gesture support for pinch, double-tap, and tap interactions

--- lib/src/utilities/components/greenfield_images_detail.dart
+++ lib/src/utilities/components/greenfield_images_detail.dart
@@ -2,6 +2,8 @@ import 'dart:io';
 
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import 'package:green_field/src/cores/router/router.dart';
 import 'package:green_field/src/utilities/components/greenfield_app_bar.dart';
 import 'package:green_field/src/utilities/extensions/theme_data_extension.dart';
 
@@ -41,10 +43,15 @@ class GreenFieldImagesDetail extends StatelessWidget {
 }
 
 class DetailScreenPageView extends StatefulWidget {
-  const DetailScreenPageView(
-      {super.key, required this.widgets, required this.initialIndex});
+  const DetailScreenPageView({
+    super.key,
+    required this.widgets,
+    required this.initialIndex,
+  });
+
   final List<Widget> widgets;
   final int initialIndex;
+
   @override
   State<DetailScreenPageView> createState() => _DetailScreenPageView();
 }
@@ -53,6 +60,10 @@ class _DetailScreenPageView extends State<DetailScreenPageView>
     with TickerProviderStateMixin {
   late PageController _pageViewController;
   late TabController _tabController;
+  final TransformationController _transformationController = TransformationController();
+  late TapDownDetails _doubleTapDetails;
+  late AnimationController _animationController;
+  Animation<Matrix4>? _animation;
 
   @override
   void initState() {
@@ -60,16 +71,45 @@ class _DetailScreenPageView extends State<DetailScreenPageView>
     _pageViewController = PageController(initialPage: widget.initialIndex);
 
     _tabController = TabController(
-        initialIndex: widget.initialIndex,
-        length: widget.widgets.length,
-        vsync: this);
+      initialIndex: widget.initialIndex,
+      length: widget.widgets.length,
+      vsync: this,
+    );
+
+    _animationController = AnimationController(
+      duration: const Duration(milliseconds: 200),
+      vsync: this,
+    )..addListener(() {
+      _transformationController.value = _animation!.value;
+    });
   }
 
   @override
   void dispose() {
-    super.dispose();
     _pageViewController.dispose();
     _tabController.dispose();
+    _transformationController.dispose();
+    _animationController.dispose();
+    super.dispose();
+  }
+
+  void _doubleTapZoom() {
+    final position = _doubleTapDetails.localPosition;
+    final endMatrix = _transformationController.value.isIdentity()
+        ? (Matrix4.identity()
+      ..translate(-position.dx, -position.dy)
+      ..scale(2.0))
+        : Matrix4.identity();
+
+    _animation = Matrix4Tween(
+      begin: _transformationController.value,
+      end: endMatrix,
+    ).animate(CurvedAnimation(
+      parent: _animationController,
+      curve: Curves.easeInOut,
+    ));
+
+    _animationController.forward(from: 0);
   }
 
   @override
@@ -77,10 +117,23 @@ class _DetailScreenPageView extends State<DetailScreenPageView>
     return Stack(
       alignment: Alignment.bottomCenter,
       children: [
-        PageView(
-            controller: _pageViewController,
-            onPageChanged: _handlePageViewChanged,
-            children: widget.widgets,
+        PageView.builder(
+          controller: _pageViewController,
+          onPageChanged: _handlePageViewChanged,
+          itemCount: widget.widgets.length,
+          itemBuilder: (context, index) {
+            return GestureDetector(
+              onTap: () => context.pop(),
+              onDoubleTapDown: (d) => _doubleTapDetails = d,
+              onDoubleTap: _doubleTapZoom,
+              child: InteractiveViewer(
+                transformationController: _transformationController,
+                minScale: 1.0,
+                maxScale: 4.0,
+                child: widget.widgets[index],
+              ),
+            );
+          },
         ),
         Padding(
           padding: const EdgeInsets.only(bottom: 40),
