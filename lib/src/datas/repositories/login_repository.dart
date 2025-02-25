a1c1d51a706112c755252493441b60a99d2683b8
bulmang <hmk113737@gmail.com>
2025-02-25 10:37:32 +0900

    [remove]: Remove unused code from login

--- lib/src/datas/repositories/login_repository.dart
+++ lib/src/datas/repositories/login_repository.dart
@@ -1,10 +1,7 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:green_field/src/datas/services/firebase_auth_service.dart';
-import 'package:firebase_auth/firebase_auth.dart' as firebase_auth;
 import 'package:green_field/src/model/token.dart';
-import 'package:kakao_flutter_sdk_user/kakao_flutter_sdk_user.dart' as kakao;
 import '../../cores/error_handler/result.dart';
 
 class LoginRepository {
@@ -38,27 +35,6 @@ class LoginRepository {
     }
   }
 
-  Future<Result<void, Exception>> signOut() async {
-    final result = await firebaseAuthService.signOut();
-
-    switch (result) {
-      case Success(value: final value):
-        return Success(value);
-      case Failure(exception: final exception):
-        return Failure(exception);
-    }
-  }
-
-  Future<Result<void, Exception>> deleteUser() async {
-    final result = await firebaseAuthService.deleteUser();
-
-    switch (result) {
-      case Success(value: final v):
-        return Success(v);
-      case Failure(exception: final exception):
-        return Failure(exception);
-    }
-  }
 }
 
 final loginRepositoryProvider = Provider<LoginRepository>((ref) {
