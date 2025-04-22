4cb235e9d2fb5e2e5368793074cbf356ed6a30e8
bulmang <hmk113737@gmail.com>
2025-04-22 15:12:29 +0900

    [refactor]: Refactor Notice feature to follow repository pattern with abstract service class and dependency injection

--- lib/src/datas/repositories/notice_repository.dart
+++ lib/src/datas/repositories/notice_repository.dart
@@ -2,24 +2,25 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:green_field/src/cores/image_type/image_type.dart';
+import 'package:green_field/src/datas/services/firebase_stores/firebase_notice_service.dart';
+import 'package:green_field/src/domains/interfaces/notice_service_interface.dart';
 import 'package:image_picker/image_picker.dart';
 
 import '../../cores/error_handler/result.dart';
 import '../../model/notice.dart';
 import '../../model/user.dart';
 import '../services/firebase_storage_service.dart';
-import '../services/firebase_stores/firebase_store_service.dart';
 
 class NoticeRepository {
-  final FirebaseStoreService firebaseStoreService;
+  final NoticeServiceInterface service;
   final FirebaseStorageService firebaseStorageService;
 
-  NoticeRepository({required this.firebaseStoreService, required this.firebaseStorageService});
+  NoticeRepository({required this.service, required this.firebaseStorageService});
 
-  /// Notice 리스트 가져오기
+  /// 공지 글 목록 조회
   Future<Result<List<Notice>, Exception>> getNoticeList(User? user) async {
     try {
-      final result = await firebaseStoreService.getNoticeList(user);
+      final result = await service.getNoticeList(user);
 
       switch(result) {
         case Success(value: final noticeList):
@@ -32,10 +33,10 @@ class NoticeRepository {
     }
   }
 
-  /// 다음 Notice 리스트 가져오기
+  /// 공지 글 추가 목록(페이징) 조회
   Future<Result<List<Notice>, Exception>> getNextNoticeList(List<Notice>? lastNotice, User user) async {
     try {
-      final result = await firebaseStoreService.getNextNoticeList(lastNotice, user);
+      final result = await service.getNextNoticeList(lastNotice, user);
 
       switch(result) {
         case Success(value: final noticeList):
@@ -48,7 +49,7 @@ class NoticeRepository {
     }
   }
 
-  /// Notice DB 생성
+  /// 공지 글 생성
   Future<Result<Notice, Exception>> createNoticeDB(User user, Notice notice, List<ImageType>? images) async {
     try {
       final uploadImageResult = await uploadImage(user, images);
@@ -56,7 +57,7 @@ class NoticeRepository {
       switch(uploadImageResult) {
         case Success(value: final imageUrls):
           notice.images = imageUrls;
-          final result = await firebaseStoreService.createNoticeDB(notice, user);
+          final result = await service.createNoticeDB(notice, user);
 
           switch (result) {
             case Success(value: final notice):
@@ -72,10 +73,10 @@ class NoticeRepository {
     }
   }
 
-  /// 특정 Notice 가져오기
+  /// 특정 공지 글 조회
   Future<Result<Notice, Exception>> getNotice(String noticeId, User user) async {
     try {
-      final result = await firebaseStoreService.getNotice(noticeId, user);
+      final result = await service.getNotice(noticeId, user);
 
       switch(result) {
         case Success(value: final noticeId):
@@ -88,10 +89,10 @@ class NoticeRepository {
     }
   }
 
-  /// Notice 문서 삭제
+  /// 공지 글 삭제
   Future<Result<void, Exception>> deleteNoticeDB(String noticeId, User user) async {
     try {
-      final result = await firebaseStoreService.deleteNoticeDB(noticeId, user);
+      final result = await service.deleteNoticeDB(noticeId, user);
 
       switch (result) {
         case Success():
@@ -140,7 +141,7 @@ class NoticeRepository {
 /// NoticeRepositoryProvider 생성
 final noticeRepositoryProvider = Provider<NoticeRepository>((ref) {
   return NoticeRepository(
-    firebaseStoreService: FirebaseStoreService(FirebaseFirestore.instance),
+    service: FirebaseStoreNoticeService(FirebaseFirestore.instance),
     firebaseStorageService: FirebaseStorageService(FirebaseStorage.instance),
   );
 });
--- lib/src/datas/services/firebase_stores/firebase_store_service.dart
+++ lib/src/datas/services/firebase_stores/firebase_store_service.dart
@@ -1,10 +1,9 @@
 import 'package:cloud_firestore/cloud_firestore.dart' as firebase_store;
 import 'package:green_field/src/cores/error_handler/result.dart';
-import 'package:green_field/src/datas/repositories/onboarding_repository.dart';
 import 'package:green_field/src/model/comment.dart';
 import 'package:green_field/src/utilities/enums/user_type.dart';
+
 import '../../../model/campus.dart';
-import '../../../model/notice.dart';
 import '../../../model/post.dart';
 import '../../../model/report.dart';
 import '../../../model/user.dart' as GFUser;
@@ -74,139 +73,6 @@ class   FirebaseStoreService {
       return Success(null); // 성공적으로 삭제되었음을 나타냄
     } catch (e) {
       return Failure(Exception('사용자 데이터 삭제 실패: $e'));
-    }
-  }
-
-
-
-  /// Notice Collection 생성 및 Notice 데이터 추가
-  Future<Result<Notice, Exception>> createNoticeDB(Notice notice, GFUser.User user) async {
-    try {
-      if(user.campus == '익명' || user.userType == getUserTypeName(UserType.student)) return Failure(Exception('인증 되지 않은 사용자입니다.'));
-      var campusDocRef = _store.collection('Campus').doc(user.campus);
-
-      await campusDocRef.collection('Notice').doc(notice.id).set(notice.toMap());
-
-      return Success(notice);
-    } catch (e) {
-      print(e);
-      return Failure(Exception('notice 데이터 생성 실패: $e'));
-    }
-  }
-
-  /// Notice Collection에서 Notice 데이터 삭제
-  Future<Result<void, Exception>> deleteNoticeDB(String noticeId, GFUser.User user) async {
-    try {
-      if(user.campus == '익명' || user.userType == getUserTypeName(UserType.student)) return Failure(Exception('인증 되지 않은 사용자입니다.'));
-      await _store.collection('Campus').doc(user.campus ?? '관악').collection('Notice').doc(noticeId).delete();
-
-      return Success(null);
-    } catch (e) {
-      print(e);
-      return Failure(Exception('notice 데이터 삭제 실패: $e'));
-    }
-  }
-
-  /// Notice Collection의 특정 Notice 데이터 업데이트
-  Future<Result<Notice, Exception>> updateNoticeDB(Notice notice, GFUser.User user) async {
-    try {
-      if(user.campus == '익명' || user.userType == getUserTypeName(UserType.student)) return Failure(Exception('인증 되지 않은 사용자입니다.'));
-      // Firestore에서 해당 문서 업데이트
-      await _store
-          .collection('Campus').doc(user.campus).collection('Notice')
-          .doc(notice.id).update(notice.toMap());
-
-      return Success(notice);
-    } catch (e) {
-      print(e);
-      return Failure(Exception('notice 데이터 업데이트 실패: $e'));
-    }
-  }
-
-  /// Notice List 가져오기
-  Future<Result<List<Notice>, Exception>> getNoticeList(GFUser.User? user) async {
-    try {
-      print("user : $user");
-      // 기본 쿼리
-      var query = _store
-          .collection('Campus').doc(user == null ? '관악' : user.campus)
-          .collection('Notice').orderBy('created_at', descending: true)
-          .limit(15);
-
-      // 첫 번째 페이지 또는 페이징 데이터를 가져오기
-      var querySnapshot = await query.get();
-
-      // 데이터를 Notice 객체로 매핑
-      List<Notice> noticeList = querySnapshot.docs
-          .map((doc) => Notice.fromMap(doc.data()))
-          .toList();
-
-      if (noticeList.isEmpty) {
-        return Success([]);
-      } else {
-        return Success(noticeList);
-      }
-    } catch (e) {
-      return Failure(Exception('공지사항 데이터 가져오기 실패: $e'));
-    }
-  }
-
-  /// Notice List 추가로 가져오기
-  Future<Result<List<Notice>, Exception>> getNextNoticeList(List<Notice>? lastNotice, GFUser.User user) async {
-    try {
-      var query = _store
-          .collection('Campus').doc(user.campus == '익명' ? '관악' : user.campus)
-          .collection('Notice').orderBy('created_at', descending: true)
-          .limit(10);
-
-      if (lastNotice != [] && lastNotice != null) {
-        final lastDoc = await _store.collection('Campus').doc(user.campus == '익명' ? '관악' : user.campus).collection('Notice')
-            .doc(lastNotice.last.id)
-            .get();
-        query = query.startAfterDocument(lastDoc);
-      }
-
-      var querySnapshot = await query.get();
-
-      List<Notice> noticeList = querySnapshot.docs
-          .map((doc) => Notice.fromMap(doc.data()))
-          .toList();
-
-      if (noticeList.isEmpty) {
-        return Success([]);
-      } else {
-        if (lastNotice != null)
-          lastNotice!.addAll(noticeList);
-
-        return Success(lastNotice ?? noticeList);
-      }
-    } catch (e) {
-      return Failure(Exception('getNextNoticeList 함수 에러: $e'));
-    }
-  }
-
-  /// 특정 Notice 가져오기
-  Future<Result<Notice, Exception>> getNotice(String noticeId, GFUser.User user) async {
-    try {
-      // Firestore에서 특정 문서 가져오기
-      final documentSnapshot = await _store
-          .collection('Campus').doc(user.campus == '익명' ? '관악' : user.campus)
-          .collection('Notice').doc(noticeId).get();
-
-      if (documentSnapshot.exists) {
-        final data = documentSnapshot.data();
-        if (data != null) {
-          // Firestore 데이터를 Notice 객체로 변환
-          final notice = Notice.fromMap({
-            ...data,
-            'id': documentSnapshot.id, // 문서 ID를 직접 추가
-          });
-          return Success(notice);
-        }
-      }
-      return Failure(Exception('데이터가 존재 하지 않습니다.'));
-    } catch (e) {
-      return Failure(Exception('공지사항 데이터 가져오기 실패: $e'));
     }
   }
 
